53d5eb4f2516cbef9059c0d5624c931136236611
fozerol <fozerol@gmail.com>
2017-07-26 09:15:05 +0300

    Newly added features

---
+++ src/main/java/com/mycompany/mavenproject3/appdao/UserRoleDao.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject3.appdao;
+
+import com.mycompany.mavenproject3.entity.Customer;
+import genericdao.GenericDaoImp;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+
+/**
+ *
+ * @author fatih
+ */
+@Stateless 
+public class CustomerDao extends GenericDaoImp<Customer>{
+    public CustomerDao() {
+        super.setType(Customer.class);
+    }
+    public CustomerDao(Class type){
+        super.setType(type);
+    }
+    public void setEntityManager(EntityManager em){
+        super.setEm(em);
+    }
+}
