d69591e695f7f40dcef5904250f464989e55dff8
fozerol <fozerol@gmail.com>
2017-09-06 13:38:04 +0300

    update Resource Bundle

--- src/main/java/com/mycompany/mavenproject3/PublicComponent.java
+++ src/main/java/com/mycompany/mavenproject3/PublicComponent.java
@@ -5,6 +5,7 @@ import com.vaadin.event.ShortcutAction;
 import com.vaadin.ui.*;
 import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.PostConstruct;
 
 /**
  * @author Alejandro Duarte.
@@ -16,34 +17,25 @@ public class PublicComponent extends CustomComponent {
         
 
     public PublicComponent() {
-        List<Language> langs = new ArrayList<>();
         
-        langs.add(new Language(1,"Turkish"));
-        langs.add(new Language(2,"English"));
-        
+    }
+    @PostConstruct
+    public void init(){
         TextField username = new TextField("Username");
-        
-        language.setItems(langs);
+        language.setItems(AuthService.langs);
         language.setEmptySelectionAllowed(false);
-        language.setValue(langs.iterator().next());
+        language.setValue(AuthService.langs.iterator().next());
         language.setItemCaptionGenerator(e-> e.getName());
         username.focus();
-
         PasswordField password = new PasswordField("Password");
-        
-
         CheckBox rememberMe = new CheckBox("Remember me");
-
         Button button = new Button("Login", e -> onLogin(username.getValue(), password.getValue(), rememberMe.getValue()));
         button.setClickShortcut(ShortcutAction.KeyCode.ENTER);
-
         FormLayout formLayout = new FormLayout(username, password, button,language,rememberMe);
         formLayout.setSizeUndefined();
-
         VerticalLayout layout = new VerticalLayout(formLayout);
         layout.setSizeFull();
         layout.setComponentAlignment(formLayout, Alignment.MIDDLE_CENTER);
-
         setCompositionRoot(layout);
         setSizeFull();
     }
--- target/generated-sources/annotations/com/mycompany/mavenproject3/entity/auth/UserRole_.java
+++ target/generated-sources/annotations/com/mycompany/mavenproject3/entity/auth/UserRole_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-08-23T17:47:37")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-09-06T12:08:46")
 @StaticMetamodel(UserRole.class)
 public class UserRole_ { 
 
